90f82363bed0ef4408098a8a6bcae615dc913c42
fweilun <william20020503@gmail.com>
2026-03-14 22:12:59 +0800

    update cors permission

--- src/lib.rs
+++ src/lib.rs
@@ -4,13 +4,22 @@ pub mod handlers;
 pub mod models;
 
 use axum::{
+    http::Method,
+    header::{AUTHORIZATION, CONTENT_TYPE},
     routing::{get, post},
     Router,
 };
+
 use database::AppState;
 use tower_http::trace::TraceLayer;
+use tower_http::cors::{Any, CorsLayer};
 
 pub fn create_router(state: AppState) -> Router {
+    let cors = CorsLayer::new()
+    .allow_origin(Any)
+    .allow_methods([Method::GET, Method::POST])
+    .allow_headers([CONTENT_TYPE, AUTHORIZATION]);
+
     Router::new()
         .route("/health", get(handlers::health_check))
         .route("/api/login", post(auth::login))
@@ -28,6 +37,7 @@ pub fn create_router(state: AppState) -> Router {
             "/api/photos/:photo_id",
             get(handlers::get_photo_handler).delete(handlers::delete_photo_handler),
         )
+        .layer(cors)
         .layer(TraceLayer::new_for_http())
         .with_state(state)
 }
